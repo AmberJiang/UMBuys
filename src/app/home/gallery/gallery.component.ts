7edd218709d91df3d3801f573c85b208d7d8e580
nanyanjiang <nanyan.jiang001@umb.edu>
2020-08-22 23:23:09 -0400

    NJ - create home component

---
+++ src/app/home/gallery/gallery.component.ts
@@ -0,0 +1,21 @@
+  /**
+   * Date: 8/22/2020
+   * Description: Gallery component class
+   * Author: Nanyan Jiang
+  */
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gallery',
+  templateUrl: './gallery.component.html',
+  styleUrls: ['./gallery.component.css']
+})
+export class GalleryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
